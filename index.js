2e7b137bc396ece5d6294436a40c3121772c1e0e
CrifluxMedia <mercymoses565@gmail.com>
2024-12-04 02:41:33 -0800

    added route

--- index.js
+++ index.js
@@ -2,28 +2,30 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
-const cloudinary = require('cloudinary').v2
-const fileupload = require('express-fileupload')
-const userRouter = require('./routes/userRouter')
+const cloudinary = require("cloudinary").v2;
+const fileupload = require("express-fileupload");
+const userRouter = require("./routes/userRouter");
+const eventRouter = require("./routes/eventRouter");
 
-  cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET, // Click 'View API Keys' above to copy your API secret
-  });
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
 
 //setting up middlewares
-app.use(fileupload({useTempFiles: true}))
+app.use(fileupload({ useTempFiles: true }));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 //routes
 app.get("/", (req, res) => {
   res.status(200).json({ success: true, message: "Mb Events server" });
 });
 app.use("/api/v1", userRouter);
+app.use("/api/v1/events", eventRouter);
 
 //error route
 app.use((req, res) => {
